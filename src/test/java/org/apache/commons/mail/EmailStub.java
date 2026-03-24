a0edfab209bd5df749d908b87bec31d424a402ad
lana-bu <lanabu@umich.edu>
2026-03-23 23:48:29 -0400

    email stub and addBcc test

---
+++ src/test/java/org/apache/commons/mail/EmailStub.java
@@ -0,0 +1,13 @@
+package org.apache.commons.mail;
+
+import java.util.Map;
+
+public class EmailStub extends Email {
+	
+	@Override
+	public Email setMsg(String msg) throws EmailException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ src/test/java/org/apache/commons/mail/EmailTest.java
@@ -0,0 +1,38 @@
+package org.apache.commons.mail;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmailTest {
+	
+	private static final String[] TEST_EMAILS = { "ab@bc.com", "a.b@c.org", 
+			"abcdefghijklmnopqrst@abcdefghijklmnopqrst.com.bd" };
+	
+//	private String[] testValidChars = { "", "a", "A", "\uc5ec", "0123456789", 
+//			"012345678901234567890", "\n" };
+	
+	private EmailStub email;
+	
+	@Before
+	public void setUpEmailTest() throws Exception {
+		email = new EmailStub();
+	}
+	
+	@After
+	public void tearDownEmailTest() throws Exception {
+		
+	}
+	
+	/*
+	 * Test addBcc(String email) function
+	 */
+	@Test
+	public void testAddBcc() throws Exception {
+		email.addBcc(TEST_EMAILS);
+		
+		assertEquals(3, email.getBccAddresses().size());
+	}
+}
